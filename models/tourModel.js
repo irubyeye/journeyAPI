79fd5af35b991f76d46d7114bf0890a8bbdb399b
irubyeye <irubyeye@gmail.com>
2023-09-15 17:55:35 +0300

    added some mongoose hooks

--- models/tourModel.js
+++ models/tourModel.js
@@ -93,6 +93,13 @@ tourSchema.post(/^find/, function (doc, next) {
   next();
 });
 
+// Aggregation middleware
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this);
+  next();
+});
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
